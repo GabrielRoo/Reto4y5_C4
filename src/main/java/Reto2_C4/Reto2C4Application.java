8d8e929f1c3bf9e38a590624c120c5d09e83e7df
Gabriel Roa <gabrielfelipert@gmail.com>
2021-12-13 19:17:48 -0500

    second commit

--- src/main/java/Reto2_C4/Reto2C4Application.java
+++ src/main/java/Reto2_C4/Reto2C4Application.java
@@ -2,10 +2,14 @@ package Reto2_C4;
 
 import Reto2_C4.Interfaces.UserInterface;
 import Reto2_C4.Interfaces.CleaningProductsInterface;
+import Reto2_C4.Interfaces.OrderInterface;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,13 +19,25 @@ public class Reto2C4Application implements CommandLineRunner {
         private CleaningProductsInterface cleaningProductsInterface;
         @Autowired
         private UserInterface userInterface;
+        @Autowired
+        private OrderInterface orderInterface;
+        @Autowired
+        private UserInterface userRepository;
+        @Autowired
+        private OrderInterface orderRepository;
+        @Autowired
+        private MongoTemplate mongoTemplate;
+        
             public static void main(String[] args) {
                     SpringApplication.run(Reto2C4Application.class, args);
 	}
         @Override
         public void run (String... args) throws Exception{
-            cleaningProductsInterface.deleteAll();
-            userInterface.deleteAll();
+        SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd");
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        cleaningProductsInterface.deleteAll();
+        userInterface.deleteAll();
+        orderInterface.deleteAll();
         }
 
 }
